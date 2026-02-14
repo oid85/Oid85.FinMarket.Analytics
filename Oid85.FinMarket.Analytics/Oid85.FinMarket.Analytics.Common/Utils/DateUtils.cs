01e0d7a5b0695c518286de2b709cc855ab9c4ae0
oid85 <oid85@mail.ru>
2026-02-14 11:22:15 +0700

    .

--- Oid85.FinMarket.Analytics/Oid85.FinMarket.Analytics.Common/Utils/DateUtils.cs
+++ Oid85.FinMarket.Analytics/Oid85.FinMarket.Analytics.Common/Utils/DateUtils.cs
@@ -20,7 +20,7 @@ namespace Oid85.FinMarket.Analytics.Common.Utils
 
         public static List<(int WeekNumber, DateOnly WeekStartDay, DateOnly WeekEndDay)> GetWeeks(DateOnly from, DateOnly to)
         {
-            var result = new List<(int, DateOnly, DateOnly)>();
+            var result = new List<(int WeekNumber, DateOnly WeekStartDay, DateOnly WeekEndDay)>();
 
             var startDate = from.ToDateTime(TimeOnly.MinValue);
             var endDate = to.ToDateTime(TimeOnly.MinValue);
@@ -35,12 +35,12 @@ namespace Oid85.FinMarket.Analytics.Common.Utils
                 var monday = startDate.AddDays((firstDayOfWeek - startDate.DayOfWeek + 7) % 7);
                 var sunday = monday.AddDays(6);
 
+                result.Add(new(weekNumber, DateOnly.FromDateTime(monday), DateOnly.FromDateTime(sunday)));
+
                 // Проверяем, входит ли воскресенье в диапазон дат
                 if (sunday > endDate)
                     break;
-
-                result.Add(new (weekNumber, DateOnly.FromDateTime(monday), DateOnly.FromDateTime(sunday)));
-
+                
                 // Переход к следующей неделе
                 startDate = sunday.AddDays(1);
             }
