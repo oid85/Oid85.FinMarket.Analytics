d36aae809ba3aeda7218808fd0f05db745f380fb
oid85 <oid85@mail.ru>
2026-03-26 22:23:03 +0700

    Update TrendStateHelper.cs

--- Oid85.FinMarket.Analytics/Oid85.FinMarket.Analytics.Application/Helpers/TrendStateHelper.cs
+++ Oid85.FinMarket.Analytics/Oid85.FinMarket.Analytics.Application/Helpers/TrendStateHelper.cs
@@ -1,21 +1,12 @@
 ﻿using Oid85.FinMarket.Analytics.Core.Enums;
 using Oid85.FinMarket.Analytics.Core.Models;
-using Oid85.FinMarket.Analytics.Core.Responses;
 
 namespace Oid85.FinMarket.Analytics.Application.Helpers
 {
     public class TrendStateHelper
     {
-        public static (TrendState TrendState, string Message) GetTrendState(List<DateValue<double>> ultimateSmoothers)
-        {
-            bool trend = ultimateSmoothers[^1].Value > ultimateSmoothers[^2].Value;
-
-            if (trend)
-                return (TrendState.Trend, "ТРЕНД");
-
-            else
-                return (TrendState.NoTrend, "НЕТ ТРЕНДА");
-        }
+        public static (TrendState TrendState, string Message) GetTrendState(List<DateValue<double>> ultimateSmoothers) => 
+            ultimateSmoothers[^1].Value > ultimateSmoothers[^2].Value ? (TrendState.Trend, "ТРЕНД") : (TrendState.NoTrend, "НЕТ ТРЕНДА");
     }
 }
 
